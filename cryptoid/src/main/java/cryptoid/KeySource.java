c7c446fefaf8eea4e45b1ff524ace41d93d18c88
Donley P'Simer <donleyp@gmail.com>
2014-11-09 22:10:37 -0800

    finishing move.

--- cryptoid/src/main/java/cryptoid/KeySource.java
+++
@@ -1,6 +0,0 @@
-package cryptoid;
-
-public interface KeySource {
-	Key getCurrentKey();
-	Key getNamedKey(String name);
-}
--- cryptoid/src/main/java/cryptoid/SimpleCipher.java
+++
@@ -1,6 +0,0 @@
-package cryptoid;
-
-public interface SimpleCipher {
-	CipherTextEnvelope encrypt(String clearText, String authenticatedData);
-	String decrypt(CipherTextEnvelope cipherText);
-}
